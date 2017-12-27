87e8c63f621567255edaeb2390534909f85982e5
Yi Ou <ouyi@gmx.de>
2017-12-27 22:36:05 +0000

    Improve the use of RequestMapping annotation

--- service-adapter/src/main/java/io/github/ouyi/jaxrs/GreetingController.java
+++ service-adapter/src/main/java/io/github/ouyi/jaxrs/GreetingController.java
@@ -5,7 +5,9 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import io.github.ouyi.jaxws.HelloClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +20,7 @@ public class GreetingController {
     @Autowired
     private HelloClient helloClient;
 
-    @RequestMapping(GREET_PATH)
+    @RequestMapping(value = GREET_PATH, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) throws MalformedURLException {
         return new Greeting(counter.incrementAndGet(), helloClient.sayHello(name));
     }
